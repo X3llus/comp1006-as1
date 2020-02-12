625d991faae64c1e730d02ed4c8d8c39744b4358
xellus <braden.coates@hotmail.com>
2020-02-11 22:35:05 -0500

    Entered data into restaurant table and make funcitonality for making table on webpage

--- index.php
+++ index.php
@@ -3,8 +3,29 @@
   <head>
     <meta charset="utf-8">
     <title>Restaurant Review</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   </head>
   <body>
 
+    <table class="table table-striped table-hover">
+      <thead><th>Restaurants</th><th>Rating</th><th>Review</th></thead>
+      <?php
+
+      $db = new PDO('mysql:host=172.31.22.43;dbname=Braden_W1095701', 'Braden_W1095701', 'P8TwvNsomx');
+
+      $select = "SELECT res.Restaurant as Restaurant, rev.rating as Rating, rev.review as Review FROM Reviews rev, Restaurants res WHERE rev.restaurant_id = res.id;";
+      $cmd = $db->prepare($select);
+      $cmd->execute();
+
+      $reviews = $cmd->fetchAll();
+
+      foreach ($reviews as $review) {
+        echo "<tr><td>" . $review["Restaurant"] . "</td><td>" . $review["Rating"] . "</td><td>" . $review["Review"] . "</td></tr>";
+      }
+
+      $db = null;
+
+      ?>
+     </table>
   </body>
 </html>
--- tables.sql
+++ tables.sql
@@ -10,3 +10,14 @@ CREATE TABLE `Braden_W1095701`.`Reviews` (
    `review` text not null,
    FOREIGN KEY (restaurant_id) REFERENCES Restaurants (id)
  );
+
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Pizza Pizza');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Boston Pizza');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Mustangs');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('TFC');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('TLC');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Oswmows');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Smokes Poutine');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('KFC');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('McDonalds');
+ INSERT INTO `Braden_W1095701`.`Restaurants` (`Restaurant`) VALUES ('Tim Hortons');
